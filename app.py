80db05055a3f5718f9c834f1c0e56838db1392cc
kenwals <kenwals@gmail.com>
2021-05-14 19:01:41 +0100

    pass user name to cookie session and welcome message

--- app.py
+++ app.py
@@ -68,8 +68,9 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
+                        session["name"] = existing_user["full-name"]
                         flash("Welcome, {}".format(
-                            request.form.get("username")))
+                            session["name"] ))
                         return redirect(url_for(
                             "profile", username=session["user"]))
             else:

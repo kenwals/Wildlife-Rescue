e7945cdc47dd693d131df23a4ef00c3f29ef523a
kenwals <kenwals@gmail.com>
2021-05-19 20:27:17 +0100

    case edit mode script
    forms is enabled for editing and buttons are toggled

---
+++ static/js/case-edit.js
@@ -0,0 +1,18 @@
+function ready() {
+
+    function formEnable() {
+        document.getElementById("fieldset1").removeAttribute("disabled");
+        document.getElementById("fieldset2").removeAttribute("disabled");
+        document.getElementById("editCaseBtn").classList.add("visually-hidden");
+        document.getElementById("saveCaseBtn").classList.remove("visually-hidden");
+      }
+    
+    document.getElementById("editCaseBtn").addEventListener("click", formEnable);
+
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
